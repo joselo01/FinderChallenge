4f7bf6a19c6726c944363317d352a02f8b4e0f08
joselo01 <sjmujica@gmail.com>
2017-03-17 17:33:29 -0500

    search
    boton buscar corregido

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,23 @@
-$('#search').keyup(function(){
-var searchField = $('#search').val();
-var myExp = new RegExp(searchField, 'i');
-$.getJSON('/books-schema.json', function(data){
-var output = '<ul id="resultado">';
-$.each(data.data, function(key, item){
-if((item.title.search(myExp) != -1) || (item.teaser.search(myExp) != -1)) {
-output +='<li>';
-output +='<span class="image"><img src=' + item.image + '/></span>';
-output +='<span class="title">'+'<h3>' + item.title+ '</h3>'+'</span>';
-output +='<span class="description">'+'<p>' + item.teaser + '</p>'+'</span>';
-output +='</li>';
-}
-});
-output += '</ul>';
-$('content').html(output);
-});
+$('#btnSearch').click(function () {
+  var searchField = $('#search').val();
+  var myExp = new RegExp(searchField, 'i');
+
+  $.getJSON('/books-schema.json', function (data) {
+    var output = '<ul id="resultado">';
+
+    $.each(data.data, function (key, item) {
+      if ((item.title.search(myExp) != -1) || (item.teaser.search(myExp) != -1)) {
+        output +='<li>';
+        output +='<span class="image"><img src=' + item.image + '/></span>';
+        output +='<span class="title">'+'<h3>' + item.title+ '</h3>'+'</span>';
+        output +='<span class="description">'+'<p>' + item.teaser + '</p>'+'</span>';
+        output +='</li>';
+      }
+    });
+
+    output += '</ul>';
+    $('content').html(output);
+  });
 });
 
 
